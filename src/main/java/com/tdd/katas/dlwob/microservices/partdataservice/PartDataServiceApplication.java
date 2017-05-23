9225f64f459ddfd7f469026039bd87609a4c7538
XYZ <idm.caz@mailinator.com>
2017-05-23 12:37:51 +0200

    Initial commit

---
+++ src/main/java/com/tdd/katas/dlwob/microservices/partdataservice/PartDataServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.katas.dlwob.microservices.partdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartDataServiceApplication.class, args);
+	}
+}
